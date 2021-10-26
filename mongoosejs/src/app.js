52c280e5e0c15bf485ffb2a2d7b07081a96d97cc
Salmankhansallu <78694695+Salmankhansallu@users.noreply.github.com>
2021-10-26 19:19:43 +0530

    commit

--- mongoosejs/src/app.js
+++ mongoosejs/src/app.js
@@ -1,2 +1,34 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/student");
+mongoose.connect("mongodb://localhost:27017/student",{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log("connection successfully..!"))
+.catch((err)=>console.log(err));
+
+
+const playlistSchema= new mongoose.Schema({
+    fname:String,
+    lname:String,
+    email:String,
+    phone:String
+})
+
+
+const Studentdata=new mongoose.model("Studentdata",playlistSchema);
+
+
+
+// createDocument=async()=>{
+//     try{
+        
+//         const third=new Studentdata({
+//             fname:"deepak",
+//             lname:"kumar",
+//             email:"deepak@gmail.com",
+//             phone:"7669815756"
+//         })
+//         const result=await Studentdata.insertMany([third]);
+//         console.log(result);
+//  }catch(err){
+//      console.log(err);
+//  }
+// }
+// createDocument();
